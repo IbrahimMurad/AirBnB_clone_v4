2718570aa0097bd8c6244b96918d9fc0ca51d3b2
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-04-05 02:55:09 +0200

    2-task

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,15 @@
+let amenities = {};
+document.addEventListener('DOMContentLoaded', function () {
+  $('input:checkbox').change(
+    function () {
+      if ($(this).is(':checked')) {
+        amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+        $('DIV.amenities h4').text(Object.values(amenities).join(', '));
+      }
+	  else {
+        delete amenities[$(this).attr('data-id')];
+        $('DIV.amenities h4').text(Object.values(amenities).join(', '));
+      }
+    }
+  );
+});
